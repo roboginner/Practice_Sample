58d7aae3886c205c2f585fd4d20a66fe92ce3657
Yuta Maeda <roboginner@yahoo.co.jp>
2016-08-01 00:44:29 +0900

    samplr8.1-1 終了！

---
+++ sample8.1-1/sample8.1-1_fact01.cs
@@ -0,0 +1,33 @@
+﻿// sample8.1-1_fact01.cs
+
+using System;
+
+class Fact
+{
+    public long CalcFact(int n)
+    {
+        long fact;
+
+        if (n == 0)
+        {
+            fact = 1;
+        }
+        else
+        {
+            fact = n * CalcFact(n - 1); // 再帰呼び出し
+        }
+        return fact;
+    }
+}
+
+class Fact01
+{
+    public static void Main()
+    {
+        Fact f = new Fact();
+        for (int i = 0; i <= 20; i++)
+        {
+            Console.WriteLine("{0}! = {1}", i, f.CalcFact(i));
+        }
+    }
+}
